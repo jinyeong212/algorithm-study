9106c3b62e93ec022d0576098b99e55b50f1ea85
임진영 <ar61208@gmail.com>
2026-07-04 22:42:17 +0900

    [level 2] Title: 점 찍기, Time: 5.59 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 임진영/프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+
+        for (long i = 0; i <= d; i += k) {
+            long maxJ = (long) Math.sqrt((long) d * d - i * i);
+
+            answer += maxJ / k + 1;
+        }
+
+        return answer;
+    }
+}

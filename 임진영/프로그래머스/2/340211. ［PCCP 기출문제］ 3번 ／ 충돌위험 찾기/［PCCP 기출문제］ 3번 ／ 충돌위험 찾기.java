c2b21b233c8bc2cbd752af184c0c85f63ce8b4c6
임진영 <ar61208@gmail.com>
2026-08-08 22:08:31 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 156.24 ms, Memory: 377 MB -BaekjoonHub

--- 임진영/프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 임진영/프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,127 +1,75 @@
 import java.util.*;
 
 class Solution {
-    int[][] points;
-    int[][] routes;
-    Queue<int[]> q;
-    int R, C;
-    HashMap<String, Integer> status;
-    
-    public int solution(int[][] points1, int[][] routes1) {
-        init(points1, routes1);
-        
-        return run();
-    }
-    
-    public int run(){
-        int result = 0;
-        
-        while(!q.isEmpty()){
-            result += isCollision();
-            
-            int size = q.size();
-            
-            for(int i = 0 ; i < size; i++){
-                int[] cur = q.poll();
-                
-                if(routes[cur[2]].length <= cur[3])
-                    cur[3]--;
-                int[] destination = points[routes[cur[2]][cur[3]] - 1];
-                
-                String key = getKey(cur[0], cur[1]);
-                int cnt = status.get(key);
-                
-                if(cnt - 1 == 0)
-                    status.remove(key);
-                else
-                    status.put(key, cnt - 1);
-                
-                //r 좌표가 다른 경우
-                if(cur[0] != destination[0]){
-                    if(cur[0] > destination[0]){
-                        cur[0]--;
-                    }else{
-                        cur[0]++;
-                    }
-                    
-                    String newKey = getKey(cur[0], cur[1]);
-                    
-                    if(status.get(newKey) == null)
-                        status.put(newKey, 0);
-            
-                    status.put(newKey, status.get(newKey) + 1);
-                    
-                    if(cur[0] == destination[0] && cur[1] == destination[1]){
-                        cur[3]++;
+    public int solution(int[][] points, int[][] routes) {
+        int answer = 0;
 
+        int robot = routes.length;
+        List<int[]>[] route = new ArrayList[robot];
 
+        // 각 로봇 이동 경로 저장
+        int maxRoute = Integer.MIN_VALUE;
+        for(int i = 0; i < robot; i++) {
+            route[i] = new ArrayList<>();
+
+            int start = routes[i][0];
+
+            route[i].add(new int[] {points[start - 1][0], points[start - 1][1]});
+
+            for(int j = 1; j < routes[i].length; j++) {
+                int end = routes[i][j];
+                int startR = points[start - 1][0];
+                int startC = points[start - 1][1];
+                int endR = points[end - 1][0];
+                int endC = points[end - 1][1];
+
+                int dr = endR - startR;
+                int dc = endC - startC;
+
+                int r = startR;
+                int c = startC;
+                while(true) {
+                    if(dr < 0) {
+                        route[i].add(new int[] {--r, c});
+                        dr++;
+                    } else if(dr > 0) {
+                        route[i].add(new int[] {++r, c});
+                        dr--;
+                    } else {
+                        if(dc < 0) {
+                            route[i].add(new int[] {r, --c});
+                            dc++;
+                        } else if(dc > 0) {
+                            route[i].add(new int[] {r, ++c});
+                            dc--;
+                        } else break;
                     }
-                    
-                    q.add(cur);
                 }
-                //c 좌표가 다른 경우
-                else if(cur[1] != destination[1]){
-                    if(cur[1] > destination[1]){
-                        cur[1]--;
-                    }else{
-                        cur[1]++;
-                    }
-                    
-                    String newKey = getKey(cur[0], cur[1]);
-                    
-                    if(status.get(newKey) == null)
-                        status.put(newKey, 0);
-            
-                    status.put(newKey, status.get(newKey) + 1);
-                    if(cur[0] == destination[0] && cur[1] == destination[1]){
-                        cur[3]++;
-                    }
-                    
-                    q.add(cur);
+
+                start = end;
+            }
+
+            maxRoute = Math.max(maxRoute, route[i].size());
+        }
+
+        int time = 0;
+        while(time < maxRoute) {
+            int[][] visited = new int[101][101];
+
+            for(int i = 0; i < robot; i++) {
+                if(time < route[i].size()) {
+                    int r = route[i].get(time)[0];
+                    int c = route[i].get(time)[1];
+
+                    if(visited[r][c] == 1) answer++;
+
+                    visited[r][c]++;
                 }
             }
+
+            time++;
         }
-        
-        
-        return result;
+
+        return answer;
     }
-    
-    public String getKey(int r, int c){
-        return r + " " + c;
-    }
-    
-    public int isCollision(){
-        int cnt = 0;
-        
-        for(String key : status.keySet()){
-            if(status.get(key) != 1)
-                cnt++;
-        }
-        
-        return cnt;
-    }
-    
-    public void init(int[][] points1, int[][] routes1){
-        points = points1;
-        routes = routes1;
-        
-        q = new ArrayDeque<int[]>();
-        status = new HashMap<String, Integer>();
-        
-        for(int i = 0 ; i < routes.length; i++){
-            int num = routes[i][0];
-            
-            int r = points[num - 1][0];
-            int c = points[num - 1][1];
-            q.add(new int[] {r, c, i, 1});
-            
-            String key = r + " " + c;
-            
-            if(status.get(key) == null)
-                status.put(key, 0);
-            
-            status.put(key, status.get(key) + 1);
-        }
-    }
-    
 }

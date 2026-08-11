7f1a2f9a07f58bbbbd878393b75bd3c8e475059f
임진영 <ar61208@gmail.com>
2026-08-11 23:49:19 +0900

    [level 2] Title: 주식가격, Time: 11.13 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 임진영/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        
+        for(int i = 0; i < prices.length; i++){
+            while(!q.isEmpty() && prices[q.peekLast()] > prices[i]){
+                int index = q.pollLast();
+                answer[index] = i - index;
+            }
+            
+            q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int index = q.poll();
+            answer[index] = prices.length - 1 - index;
+        }
+        
+        return answer;
+    }
+}

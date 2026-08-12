5bc414afa1c9861cfc127ed3ebf33f2cfe0dd73d
임진영 <ar61208@gmail.com>
2026-08-12 22:23:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 30.62 ms, Memory: 122 MB -BaekjoonHub

--- 임진영/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 임진영/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,89 +1,26 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    HashMap<Integer, Integer> map;
-    PriorityQueue<Integer> asc;
-    PriorityQueue<Integer> desc;
-    
-    public int[] solution(String[] operations) throws Exception {
-        init();
-        
-        for(String str : operations){
-            StringTokenizer st = new StringTokenizer(str);
-            
-            String command = st.nextToken();
-            
-            if(command.equals("I"))
-                insert(Integer.parseInt(st.nextToken()));
-            else{
-                if(st.nextToken().equals("1"))
-                    removeMax();
-                else
-                    removeMin();
+    public int[] solution(String[] operations) {
+        Queue<Integer> minpq = new PriorityQueue<>();
+        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            if (operation.startsWith("I ")) {
+                int n = Integer.parseInt(operation.substring(2));
+                minpq.offer(n);
+                maxpq.offer(n);
+            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
+                maxpq.remove(minpq.poll());
+            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
+                minpq.remove(maxpq.poll());
             }
-                
         }
-        
-        return getAnswer();
-    }
-    
-    public void insert(int num){
-        asc.add(num);
-        desc.add(num);
-        
-        if(map.get(num) == null)
-            map.put(num, 0);
-        
-        map.put(num, map.get(num) + 1);
-    }
-    
-    public int[] getAnswer(){
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-        
-        for(int num : map.keySet()){
-            if(map.get(num) == 0) continue;
-            
-            max = Math.max(num, max);
-            min = Math.min(num, min); 
+
+        if (minpq.isEmpty() && maxpq.isEmpty()) {
+            return new int[]{0, 0};
         }
-        
-        if(max < min)
-            return new int[] {0, 0};
-        
-        return new int[] {max, min};
-    }
-    
-    public void removeMax(){
-        while(!desc.isEmpty()){
-            int max = desc.poll();
-            
-            if(map.get(max) == 0)
-                continue;
-            
-            map.put(max, map.get(max) - 1);
-            
-            break;
-        }
-    }
-    
-    public void removeMin(){
-        while(!asc.isEmpty()){
-            int min = asc.poll();
-            
-            if(map.get(min) == 0)
-                continue;
-            
-            map.put(min, map.get(min) - 1);
-            
-            break;
-        }
-    }
-    
-    public void init(){
-        map = new HashMap<Integer, Integer>();
-        asc = new PriorityQueue<Integer>();
-        desc = new PriorityQueue<Integer>((n1, n2) -> Integer.compare(n2, n1));
+
+        return new int[]{maxpq.poll(), minpq.poll()};
     }
 }

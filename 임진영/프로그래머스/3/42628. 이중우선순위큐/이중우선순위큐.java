45b671c958c792d60842b630640df37639fafb86
임진영 <ar61208@gmail.com>
2026-08-12 22:30:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 29.90 ms, Memory: 131 MB -BaekjoonHub

--- 임진영/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 임진영/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,26 +1,97 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    public int[] solution(String[] operations) {
-        Queue<Integer> minpq = new PriorityQueue<>();
-        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
-
+    HashMap<Integer, Integer> map;
+    PriorityQueue<Integer> asc;
+    PriorityQueue<Integer> desc;
+    
+    public int[] solution(String[] operations) throws Exception {
+        init();
+        
         for (String operation : operations) {
             if (operation.startsWith("I ")) {
                 int n = Integer.parseInt(operation.substring(2));
-                minpq.offer(n);
-                maxpq.offer(n);
-            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
-                maxpq.remove(minpq.poll());
-            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
-                minpq.remove(maxpq.poll());
+                insert(n);
+            } else if (operation.equals("D -1")) {
+                removeMin();
+            } else{
+                removeMax();
             }
         }
-
-        if (minpq.isEmpty() && maxpq.isEmpty()) {
-            return new int[]{0, 0};
+        
+        return getAnswer();
+    }
+    
+    public void insert(int num){
+        asc.add(num);
+        desc.add(num);
+        
+        if(map.get(num) == null)
+            map.put(num, 0);
+        
+        map.put(num, map.get(num) + 1);
+    }
+    
+    public int[] getAnswer(){
+        int max = 0;
+        int min = 0;
+        
+        while(!desc.isEmpty()){
+            int temp = desc.poll();
+            
+            if(map.get(temp) == 0)
+                continue;
+            
+            max = temp;
+            
+            break;
         }
-
-        return new int[]{maxpq.poll(), minpq.poll()};
+        
+        while(!asc.isEmpty()){
+            int temp = asc.poll();
+            
+            if(map.get(temp) == 0)
+                continue;
+            
+            min = temp;
+            
+            break;
+        }
+        
+        
+        return new int[] {max, min};
+    }
+    
+    public void removeMax(){
+        while(!desc.isEmpty()){
+            int max = desc.poll();
+            
+            if(map.get(max) == 0)
+                continue;
+            
+            map.put(max, map.get(max) - 1);
+            
+            break;
+        }
+    }
+    
+    public void removeMin(){
+        while(!asc.isEmpty()){
+            int min = asc.poll();
+            
+            if(map.get(min) == 0)
+                continue;
+            
+            map.put(min, map.get(min) - 1);
+            
+            break;
+        }
+    }
+    
+    public void init(){
+        map = new HashMap<Integer, Integer>();
+        asc = new PriorityQueue<Integer>();
+        desc = new PriorityQueue<Integer>((n1, n2) -> Integer.compare(n2, n1));
     }
 }

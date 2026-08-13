851dd591b4fa94dd27b1ff1b87877fba57757eb2
임진영 <ar61208@gmail.com>
2026-08-13 23:34:35 +0900

    [level 2] Title: H-Index, Time: 5.06 ms, Memory: 89.1 MB -BaekjoonHub

--- 임진영/프로그래머스/2/42747. H－Index/H－Index.java
+++ 임진영/프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,14 +4,10 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
 
-        int n = citations.length;
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
 
-        for (int i = 0; i < n; i++) {
-            int h = n - i;
-
-            if (citations[i] >= h) {
-                return h;
-            }
+            if (citations[i] >= h)  return h;
         }
 
         return 0;
